6e9b2d02b07fa7b474291f98e515c7a28360acde
Boris <borisimo.work@gmail.com>
2024-09-04 17:56:01 +0300

    --add: README.md

--- L0/target/debug/L0.d
+++
@@ -1,1 +0,0 @@
-C:\Users\boris\RustroverProjects\wildberries-course\L0\target\debug\L0.exe: C:\Users\boris\RustroverProjects\wildberries-course\L0\src\main.rs
--- L0/target/debug/deps/L0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\boris\RustroverProjects\wildberries-course\L0\target\debug\deps\L0.exe: src\main.rs
-
-C:\Users\boris\RustroverProjects\wildberries-course\L0\target\debug\deps\L0.d: src\main.rs
-
-src\main.rs:
