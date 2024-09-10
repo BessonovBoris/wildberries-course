ae8cc5d5f36321880f8da5238db9d0ae5a4f02ea
Boris <borisimo.work@gmail.com>
2024-09-10 12:56:39 +0300

    add: task l1-1

---
+++ L1/L1-1/src/main.rs
@@ -0,0 +1,18 @@
+trait Action {
+    fn say(&self);
+}
+
+struct Person {
+    name: String,
+}
+
+impl Action for Person {
+    fn say(&self) {
+        println!("Hello, {}", self.name);
+    }
+}
+
+fn main() {
+    let man = Person { name: "Petr".to_string() };
+    man.say();
+}
