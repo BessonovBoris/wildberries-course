bce6b9f905676c66aaf67c1d965ec019314e3ad2
Boris <borisimo.work@gmail.com>
2024-09-19 12:44:56 +0300

    add: task L1-6

--- L1/L1-6/src/main.rs
+++ L1/L1-6/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn read_working_time() -> Result<u64, String> {
+    let mut input = String::new();
+
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => {}
+        Err(err) => return Err(err.to_string())
+    }
+
+    let num = match input.trim().parse::<u64>() {
+        Ok(num) => num,
+        Err(err) => return Err(err.to_string())
+    };
+
+    Ok(num)
+}
+
+#[tokio::main]
+async fn main() {
+    let working_time = read_working_time().unwrap();
+    let (tx, rx) = mpsc::channel();
+    let (shutdown_tx, shutdown_rx) = mpsc::channel();
+
+    tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_secs(working_time)).await;
+        shutdown_tx.send(()).unwrap();
+    });
+    tokio::spawn(producer(tx, shutdown_rx));
+    tokio::spawn(consumer(rx)).await.unwrap();
+}
+
+async fn producer(tx: mpsc::Sender<i32>, shutdown_rx: mpsc::Receiver<()>) {
+    let mut i = 1;
+    loop {
+        if let Ok(_) = shutdown_rx.try_recv() {
+            break;
+        }
+
+        tx.send(i).unwrap();
+        i += 1;
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+async fn consumer(rx: mpsc::Receiver<i32>) {
+    loop {
+        let num = match rx.recv() {
+            Ok(n) => n,
+            Err(_) => break,
+        };
+        println!("Received {}", num);
+    }
 }
