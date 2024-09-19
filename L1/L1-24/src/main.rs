c94ccd671359fa7904595a2470324a9a98111372
Boris <borisimo.work@gmail.com>
2024-09-19 19:20:50 +0300

    add: task L1-24

--- L1/L1-24/src/main.rs
+++ L1/L1-24/src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let s = "asdfA".to_string();
+    println!("{}", uniq_symbol(s));
+}
+
+fn uniq_symbol(s: String) -> bool {
+    let mut set = HashSet::new();
+    for c in s.to_lowercase().chars() {
+        if !set.insert(c) {
+            return false;
+        }
+    };
+
+    true
 }
