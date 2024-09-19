a1669d45618a8d2d35da29a8eec73ec51ca5a05f
Boris <borisimo.work@gmail.com>
2024-09-19 19:25:57 +0300

    add: task L1-23

--- L1/L1-23/src/main.rs
+++ L1/L1-23/src/main.rs
@@ -1,3 +1,21 @@
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+
+    fn distance(&self, p: &Point) -> f64 {
+        ((self.x - p.x).powi(2) + (self.y - p.y).powi(2)).sqrt()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point::new(0.0, 0.0);
+    let p2 = Point::new(3.0, 4.0);
+
+    println!("{}", p1.distance(&p2));
 }
