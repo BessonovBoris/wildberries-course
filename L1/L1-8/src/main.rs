ff45488122e70720148ab5949289d601379733c0
Boris <borisimo.work@gmail.com>
2024-09-19 14:29:23 +0300

    add: task L1-8

--- L1/L1-8/src/main.rs
+++ L1/L1-8/src/main.rs
@@ -1,3 +1,49 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::sync::Arc;
+use dashmap::DashMap;
+
+fn realisation_hashmap() {
+    let map = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = Vec::new();
+
+    for i in 0..10 {
+        let map = map.clone();
+        handles.push(std::thread::spawn(move || {
+            map.lock().unwrap().insert(format!("Number-{i}"), i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("{:?}", map.lock().unwrap());
+}
+
+fn realisation_dashmap() {
+    let dm = DashMap::new();
+    let dm = Arc::new(dm);
+
+    let mut handles = Vec::new();
+    for i in 0..5 {
+        let dm_clone = dm.clone();
+        handles.push(std::thread::spawn(move || {
+            dm_clone.insert(format!("Number-{}", i), i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("{:?}", dm);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hash Map: ");
+    realisation_hashmap();
+
+    println!("Dash Map: ");
+    realisation_dashmap();
 }
