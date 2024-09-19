51a6cad0fd1c833cc5a3096e94c545888dd9a325
Boris <borisimo.work@gmail.com>
2024-09-19 16:26:36 +0300

    add: task L1-16

--- L1/L1-16/src/main.rs
+++ L1/L1-16/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let data = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    let target = 11;
+
+    let result = data.binary_search(&target);
+
+    match result {
+        Ok(index) => println!("Found {} at index {}", target, index),
+        Err(_) => println!("{} not found in the array", target),
+    }
 }
