17dc7df327bc9157a63a3d4e2ef58c773b8b35e4
Boris <borisimo.work@gmail.com>
2024-09-19 14:50:06 +0300

    add: task L1-9

--- L1/L1-9/src/main.rs
+++ L1/L1-9/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let num = 4i64;
+
+    println!("{}", bit_to_1(num, 2, false));
+}
+
+fn bit_to_1(num: i64, i: usize, to_one: bool) -> i64 {
+    if to_one {
+        let tmp = 1 << i;
+        let ans = num | tmp;
+
+        return ans
+    }
+
+    let tmp = i64::MAX - (1 << i);
+    let ans = num & tmp;
+
+    ans
 }
