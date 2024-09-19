072b368d8be26aa16898a671f8ca81529c25973a
Boris <borisimo.work@gmail.com>
2024-09-19 17:45:45 +0300

    add: task L1-19

--- L1/L1-19/src/main.rs
+++ L1/L1-19/src/main.rs
@@ -1,3 +1,19 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 fn main() {
-    println!("Hello, world!");
+    let s = reverse_words("hello world".to_string());
+    println!("{}", s);
+}
+
+fn reverse_words(s: String) -> String {
+    let vec_words = s.split(' ').collect::<Vec<&str>>();
+    let mut ans = String::new();
+
+    for word in vec_words.iter() {
+        let s = word.graphemes(true).rev().collect::<String>();
+        ans.push_str(format!("{} ", s).as_str());
+    }
+    ans.pop();
+
+    ans
 }
