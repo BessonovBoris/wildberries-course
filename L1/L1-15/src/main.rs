9a54055cd32eb515b42d9682ec775c2cd2cddf43
Boris <borisimo.work@gmail.com>
2024-09-19 16:25:14 +0300

    add: task L1-15

--- L1/L1-15/src/main.rs
+++ L1/L1-15/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut data = vec![3, 1, 4, 1, 5, 9, 2, 6];
+    // sort use the quicksort algorithm
+    data.sort();
+    println!("{:?}", data);
 }
