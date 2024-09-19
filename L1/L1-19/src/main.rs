11dac00a0d2e7dd88ae17c739d40eb1a6ce43d20
Boris <borisimo.work@gmail.com>
2024-09-19 17:50:59 +0300

    add: task L1-19

--- L1/L1-19/src/main.rs
+++ L1/L1-19/src/main.rs
@@ -1,19 +1,8 @@
-use unicode_segmentation::UnicodeSegmentation;
-
 fn main() {
-    let s = reverse_words("hello world".to_string());
+    let s = reverse_words("snow dog sun".to_string());
     println!("{}", s);
 }
 
 fn reverse_words(s: String) -> String {
-    let vec_words = s.split(' ').collect::<Vec<&str>>();
-    let mut ans = String::new();
-
-    for word in vec_words.iter() {
-        let s = word.graphemes(true).rev().collect::<String>();
-        ans.push_str(format!("{} ", s).as_str());
-    }
-    ans.pop();
-
-    ans
+    s.split(' ').rev().collect::<Vec<&str>>().join(" ")
 }
