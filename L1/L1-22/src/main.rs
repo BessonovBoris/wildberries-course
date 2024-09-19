a284c07421fc4319736737f34cfd7fcb5925dbf0
Boris <borisimo.work@gmail.com>
2024-09-19 19:30:41 +0300

    add: task L1-22

--- L1/L1-22/src/main.rs
+++ L1/L1-22/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vec = Vec::from_iter(0..=10);
+    let i = 10;
+
+    if i >= vec.len() {
+        println!("WARNING: Index out of bounds");
+        return;
+    }
+
+    vec.remove(i);
+
+    println!("{:#?}", vec);
 }
