db89e06ea8773fa928d7b2534ddfe24aea353682
Boris <borisimo.work@gmail.com>
2024-09-19 15:03:08 +0300

    add: task L1-10

--- L1/L1-10/src/main.rs
+++ L1/L1-10/src/main.rs
@@ -1,3 +1,44 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    let (calculus_tx, calculus_rx) = mpsc::channel();
+
+    let vector_reader = thread::spawn(move || {
+        let n = 10;
+        let vec = Vec::from_iter(0..=n);
+
+        for i in vec {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    let calculating = thread::spawn(move || {
+        loop {
+            let num = match rx.recv() {
+                Ok(n) => n,
+                Err(_) => break,
+            };
+
+            calculus_tx.send(num * num).unwrap();
+        }
+    });
+
+    let output = thread::spawn(move || {
+        loop {
+            let num = match calculus_rx.recv() {
+                Ok(n) => n,
+                Err(_) => break,
+            };
+
+            println!("{}", num);
+        }
+    });
+
+    vector_reader.join().unwrap();
+    calculating.join().unwrap();
+    output.join().unwrap();
 }
