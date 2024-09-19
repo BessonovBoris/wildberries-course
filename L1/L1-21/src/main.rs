65560a80a0cdf7fcb03faf7ffedca7af94d158fd
Boris <borisimo.work@gmail.com>
2024-09-19 20:02:20 +0300

    add: task L1-21

--- L1/L1-21/src/main.rs
+++ L1/L1-21/src/main.rs
@@ -1,3 +1,18 @@
+use num_bigint::BigInt;
+
 fn main() {
-    println!("Hello, world!");
+    let a = BigInt::from((2 << 20) + 10);
+    let b = BigInt::from((2 << 20) + 20);
+
+    let multiply = a.clone() * b.clone();
+    println!("a * b = {}", multiply);
+
+    let quotient = a.clone() / b.clone();
+    println!("a / b = {}", quotient);
+
+    let sum = a.clone() + b.clone();
+    println!("a + b = {}", sum);
+
+    let difference = a.clone() - b.clone();
+    println!("a - b = {}", difference);
 }
