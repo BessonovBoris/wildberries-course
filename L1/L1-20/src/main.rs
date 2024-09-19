4bce872a74c7fc99cd587f6073f8d2e643cd5de1
Boris <borisimo.work@gmail.com>
2024-09-19 18:35:56 +0300

    add: task L1-20

--- L1/L1-20/src/main.rs
+++ L1/L1-20/src/main.rs
@@ -1,3 +1,49 @@
+trait ClientInterface {
+    fn execute(&self);
+}
+
+struct Client;
+
+impl ClientInterface for Client {
+    fn execute(&self) {
+        println!("hello world from client");
+    }
+}
+
+struct Service;
+
+impl Service {
+    fn do_something(&self) {
+        println!("something from service");
+    }
+}
+
+struct Adapter {
+    adaptee: Service,
+}
+
+impl Adapter {
+    fn new(adaptee: Service) -> Self {
+        Self { adaptee }
+    }
+}
+
+impl ClientInterface for Adapter {
+    fn execute(&self) {
+        self.adaptee.do_something();
+    }
+}
+
+fn call_method(client: impl ClientInterface) {
+    client.execute();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client;
+    let service = Service;
+    let adapter = Adapter::new(service);
+
+    call_method(adapter);
+    call_method(client);
+    // call_method(service);    Service didn't implement ClientInterface
 }
