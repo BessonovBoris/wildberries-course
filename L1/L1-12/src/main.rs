92e392f3de6d04d0f5a8c7ccd823b621da981a99
Boris <borisimo.work@gmail.com>
2024-09-19 15:31:15 +0300

    add: task L1-12

--- L1/L1-12/src/main.rs
+++ L1/L1-12/src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
+fn intersection_v1<T: Clone>(a: HashSet<T>, b: HashSet<T>) -> HashSet<T>
+where T: Eq, T: Hash {
+    a.intersection(&b).cloned().collect()
+}
+
+fn intersection_v2<T>(a: HashSet<T>, b: HashSet<T>) -> HashSet<T>
+where T: Eq, T: Hash {
+    let mut ans = HashSet::new();
+
+    for i in a {
+        if b.contains(&i) {
+            ans.insert(i);
+        }
+    }
+
+    ans
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = HashSet::from_iter(vec![1, 2, 3]);
+    let b = HashSet::from_iter(vec![2, 3, 4, 5]);
+
+    // let c = intersection_v1(a, b);
+    let c = intersection_v2(a, b);
+
+    println!("{:?}", c);
 }
