0b59a41d3fe98bc48b54418c0852270894056e04
Boris <borisimo.work@gmail.com>
2024-09-24 21:09:10 +0300

    fix: algorithm

--- L2/L2-2/src/lib.rs
+++ L2/L2-2/src/lib.rs
@@ -1,17 +1,22 @@
 pub fn unpacking(input: &String) -> Result<String, String> {
     let mut answer = String::new();
-    let mut escaped = false;
 
     let mut i = 0;
     while i < input.len() {
+        let mut escaped = false;
+
         if input.chars().nth(i).unwrap() == '\\' {
             i += 1;
+            if i == input.len() {
+                return Err(String::from("wrong format"));
+            }
+
             escaped = true;
         }
         let char = input.chars().nth(i).unwrap();
 
         if char.is_digit(10) && !escaped {
-            return Err("is not a digit".to_string());
+            return Err(String::from("wrong format"));
         }
 
         let mut j = i + 1;
--- L2/L2-2/src/main.rs
+++ L2/L2-2/src/main.rs
@@ -3,6 +3,8 @@ use L2_2::unpacking;
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
+    input.pop();
+    input.pop();
 
     let s = unpacking(&input);
     println!("{}", s.unwrap());
