783dbf450748722a9b8284b21ac285ae5e2ad8b3
Boris <borisimo.work@gmail.com>
2024-10-02 12:52:42 +0300

    fix: better scalability

---
+++ L2/L2-1/src/lib.rs
@@ -0,0 +1,83 @@
+use std::io::BufRead;
+
+pub struct Program {
+    lines: Vec<String>,
+    count_words: Option<fn(&Program) -> usize>,
+    count_lines: Option<fn(&Program) -> usize>,
+    count_chars: Option<fn(&Program) -> usize>,
+}
+
+impl Program {
+    pub fn new(lines: Vec<String>) -> Self {
+        Program { lines, count_words: None, count_lines: None, count_chars: None }
+    }
+
+    pub fn with_file_name(file: String) -> Self {
+        // open file and read it to buffer
+        let file = std::io::BufReader::new(std::fs::File::open(file).unwrap());
+
+        // read 'Ok' lines and collect them into Strings
+        let s = file.lines().map_while(|ok| ok.ok()).collect::<Vec<String>>();
+
+        Program::new(s)
+    }
+
+    pub fn add_count_words(&mut self) {
+        if self.count_words.is_none() {
+            self.count_words = Some(Program::count_words)
+        }
+    }
+
+    pub fn add_count_lines(&mut self) {
+        if self.count_lines.is_none() {
+            self.count_lines = Some(Program::count_lines)
+        }
+    }
+
+    pub fn add_count_chars(&mut self) {
+        if self.count_chars.is_none() {
+            self.count_chars = Some(Program::count_chars)
+        }
+    }
+
+    pub fn run(&self) -> Vec<usize> {
+        let mut output = vec![];
+
+        if let Some(count) = self.count_lines {
+            output.push(count(self));
+        }
+
+        if let Some(count) = self.count_words {
+            output.push(count(self));
+        }
+
+        if let Some(count) = self.count_chars {
+            output.push(count(self));
+        }
+
+        output
+    }
+
+    fn count_words(&self) -> usize {
+        let mut ans = 0;
+
+        for line in self.lines.iter() {
+            ans += line.split_whitespace().count();
+        }
+
+        ans
+    }
+
+    fn count_lines(&self) -> usize {
+        self.lines.len()
+    }
+
+    fn count_chars(&self) -> usize {
+        let mut ans = 0;
+        for line in self.lines.iter() {
+            ans += line.chars().count();
+        }
+
+        ans
+    }
+}
--- L2/L2-1/src/main.rs
+++ L2/L2-1/src/main.rs
@@ -1,47 +1,4 @@
-use std::io::BufRead;
-
-struct Program {
-    lines: Vec<String>,
-}
-
-impl Program {
-    fn new(lines: Vec<String>) -> Self {
-        Program { lines, }
-    }
-
-    fn with_file_name(file: String) -> Self {
-        // open file and read it to buffer
-        let file = std::io::BufReader::new(std::fs::File::open(file).unwrap());
-
-        // read 'Ok' lines and collect them into Strings
-        let s = file.lines().filter_map(|ok| ok.ok()).collect::<Vec<String>>();
-
-        Program::new(s)
-    }
-
-    fn word_count(&self) -> usize {
-        let mut ans = 0;
-
-        for line in self.lines.iter() {
-            ans += line.split_whitespace().count();
-        }
-
-        ans
-    }
-
-    fn lines_count(&self) -> usize {
-        self.lines.len()
-    }
-
-    fn char_count(&self) -> usize {
-        let mut ans = 0;
-        for line in self.lines.iter() {
-            ans += line.chars().count();
-        }
-
-        ans
-    }
-}
+use L2_1::Program;
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
@@ -50,22 +7,23 @@ fn main() {
     }
 
     let filename = args.last().unwrap().to_string();
-    let program = Program::with_file_name(filename.clone());
-    let mut output = Vec::new();
+    let mut program = Program::with_file_name(filename.clone());
 
-    for i in 1..args.len()-1 {
-        if args[i].eq("-c") {
-            output.push(program.char_count());
-        } else if args[i].eq("-l") {
-            output.push(program.lines_count());
-        } else if args[i].eq("-w") {
-            output.push(program.word_count());
+    if args.len() == 2 {
+        program.add_count_words();
+    }
+
+    for flag in args.iter().skip(1)  {
+        if flag.eq("-c") {
+            program.add_count_chars();
+        } else if flag.eq("-l") {
+            program.add_count_lines();
+        } else if flag.eq("-w") {
+            program.add_count_words();
         }
     }
 
-    if args.len() == 2 {
-        output.push(program.word_count());
-    }
+    let output = program.run();
 
     for num in output.iter() {
         print!("{} ", num);
